87851a50f88ee77c132750911dbb2df63ceea053
Samat <samathakimullin77@gmail.com>
2026-05-22 10:44:55 +0500

    third

---
+++ src/admin/update-status.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user']) || $_SESSION['role'] != 'admin') {
+    header("Location: /public/login.php");
+    exit;
+}
+
+require_once "../../config/connect-db.php";
+
+$order_id  = (int) $_POST['order_id'];
+$status_id = (int) $_POST['status_id'];
+
+mysqli_query($conn, "UPDATE orders SET id_status = $status_id WHERE id_order = $order_id");
+
+header("Location: index.php");
+exit;
